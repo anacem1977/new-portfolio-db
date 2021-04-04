c5d301bdc356a8bfab0188a6d1fc5e991b92bd93
anace-m <maldonadoanaceciliac@johndeere.com>
2021-04-04 13:29:46 -0500

    installed dependencies

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const methodOverride = require("method-override");
+const routes = require ("./routes");
+
+const corsOptions = {
+    origin: ['http://localhost:3000'], //port for React app
+    // origin: ['http://beer-encyclopaedia.surge.sh'], //port for React app
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true, //allows session cookies to be sent back and forth
+    optionsSuccessStatus: 200 //legacy browsers
+  }
+
+app.use(cors(corsOptions))
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    console.log("I run for all routes");
+    next();
+  });
+
+//   app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "http://beer-encyclopaedia.surge.sh");
+//     res.header(
+//       "Access-Control-Allow-Headers",
+//       "Origin, X-Requested-With, Content-Type, Accept"
+//     );
+//     next();
+//   })
+
+app.use(methodOverride("_method"));
+
+app.listen(process.env.PORT||3005, () => {
+    console.log("My portfolio is set up and running");
+});
